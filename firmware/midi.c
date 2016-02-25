19e51f20fb36c7be67d291a43afbf8171cbc2f7f
Jordi Orlando <jordi.orlando@gmail.com>
2016-02-25 17:34:32 -0600

    Move UART interrupt enable into initMIDI()

--- firmware/midi.c
+++ firmware/midi.c
@@ -4,4 +4,9 @@ void initMIDI() {
   // 0x60 (96) gives a baud rate of 31.25 Kbaud
   // 48MHz / 96 / 16 = 31.25KHz
   initUART(BASE_FREQ / (31250 * 16));
+
+  // Enable the UART interrupt
+  NVIC_EnableIRQ(UART_IRQn);
+  // Enable RBR interrupt (sec 13.5.4)
+  LPC_UART->IER |= BIT0;
 }
