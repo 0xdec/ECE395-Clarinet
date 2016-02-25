feea3a7f834f6e31e473999dbe4d843a47913af5
Jordi Orlando <jordi.orlando@gmail.com>
2016-02-25 17:33:31 -0600

    Make delay functions inline

--- firmware/delay.c
+++ firmware/delay.c
@@ -1,11 +1,11 @@
 #include "delay.h"
 
-void delay_us(int delay) {
+inline void delay_us(int delay) {
   delay *= 48;
   while (delay--);
 }
 
-void delay_ms(int delay) {
+inline void delay_ms(int delay) {
   while (delay--) {
     delay_us(1000);
   }
