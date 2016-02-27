07918bc0ecc6f947b40c8691819535669a63b917
Jordi Orlando <jordi.orlando@gmail.com>
2016-02-26 20:04:39 -0600

    Fix formatting

--- firmware/notes.c
+++ firmware/notes.c
@@ -1,11 +1,11 @@
 #include "notes.h"
 
 static const uint16_t noteMap[128] = {
-0x0000
+  0x0000
 };
 
 void noteOn(uint8_t note) {
- //shiftOut(noteMap[note]);
+  //shiftOut(noteMap[note]);
 }
 
 void noteOff(uint8_t note) {
