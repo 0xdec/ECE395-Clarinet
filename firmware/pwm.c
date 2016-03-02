29c11629900cd5c76211894a61c0922a6c79dfd8
Jordi Orlando <jordi.orlando@gmail.com>
2016-03-02 00:31:20 -0600

    Rearrange timer initialization config settings

--- firmware/pwm.c
+++ firmware/pwm.c
@@ -14,17 +14,16 @@ void PWM_init(uint16_t period) {
   LPC_TMR16B0->PR = 48;
   // The TC will be reset if MR1 matches it (sec 18.7.6)
   LPC_TMR16B0->MCR = BIT4;
+  // Timer counter match value for period (sec 18.7.7)
+  LPC_TMR16B0->MR1 = period;
+  // Timer counter match value for pulse width (sec 18.7.7)
+  LPC_TMR16B0->MR0 = period;
   // Set CT16B0_MAT0 to 1 on match (sec 18.7.10)
   LPC_TMR16B0->EMR |= BIT5;
   // Select timer mode (sec 18.7.11)
   LPC_TMR16B0->CCR = 0;
   // PWM mode is enabled for CT16B0_MAT0 (sec 18.7.12)
   LPC_TMR16B0->PWMC = BIT0;
-
-  // Timer counter match value for period (sec 18.7.7)
-  LPC_TMR16B0->MR1 = period;
-  // Timer counter match value for pulse width (sec 18.7.7)
-  LPC_TMR16B0->MR0 = period;
 }
 
 void PWM_disable() {
