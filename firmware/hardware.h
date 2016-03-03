b4ad3a1299abfb29d231481a8df97daeb419106b
Jordi Orlando <jordi.orlando@gmail.com>
2016-03-03 03:11:02 -0600

    Beginnings of hardware abstraction

---
+++ firmware/hardware.h
@@ -0,0 +1,15 @@
+#ifndef _HARDWARE_H_
+#define _HARDWARE_H_
+
+#define PWM_PIN   PIO0_8
+
+#define SSEL_PIN  PIO0_2
+#define MISO_PIN  PIO0_8
+#define MOSI_PIN  PIO0_9
+#define SCK_PIN   PIO0_10
+#define PWM_PIN   PIO0_8
+
+#define RXD_PIN   PIO1_6
+#define TXD_PIN   PIO1_7
+
+#endif
