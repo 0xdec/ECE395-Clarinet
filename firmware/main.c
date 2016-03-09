359f400a8485040f34d179e4db5a87a1c1b52c51
Jordi Orlando <jordi.orlando@gmail.com>
2016-03-08 18:16:02 -0600

    Use soft tabs

--- firmware/main.c
+++ firmware/main.c
@@ -8,14 +8,14 @@
 #define ANGLE 90
 
 int main() {
-	uint8_t i = 0;
+  uint8_t i = 0;
   system_init();
-	SPI_init();
-	
+  SPI_init();
+
   while (1) {
-		SPI_send(i);
-		delay_ms(100);
-		i++;
+    SPI_send(i);
+    delay_ms(100);
+    i++;
   }
 
   return 0;
