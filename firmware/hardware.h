d0fd72a78e2eb1e6bf618e0ec907bbcadfc5c715
Jordi Orlando <jordi.orlando@gmail.com>
2016-03-08 23:45:37 -0600

    Update pin definitions

--- firmware/hardware.h
+++ firmware/hardware.h
@@ -1,15 +1,14 @@
 #ifndef _HARDWARE_H_
 #define _HARDWARE_H_
 
-#define PWM_PIN   PIO0_8
+#define PWM_PIN   LPC_IOCON->PIO0_8
 
-#define SSEL_PIN  PIO0_2
-#define MISO_PIN  PIO0_8
-#define MOSI_PIN  PIO0_9
-#define SCK_PIN   PIO0_10
-#define PWM_PIN   PIO0_8
+#define SSEL_PIN  LPC_IOCON->PIO0_2
+#define SCK_PIN   LPC_IOCON->PIO0_6
+#define MISO_PIN  LPC_IOCON->PIO0_8
+#define MOSI_PIN  LPC_IOCON->PIO0_9
 
-#define RXD_PIN   PIO1_6
-#define TXD_PIN   PIO1_7
+#define RXD_PIN   LPC_IOCON->PIO1_6
+#define TXD_PIN   LPC_IOCON->PIO1_7
 
 #endif
