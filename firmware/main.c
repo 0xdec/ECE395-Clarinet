593a14dbea32dbe1da423802920b11b12cd37945
Adam Long <arlong2@illinois.edu>
2016-03-08 18:07:07 -0600

    Counting up

--- firmware/main.c
+++ firmware/main.c
@@ -1,20 +1,21 @@
 #include "header.h"
 
 #include "system.h"
-#include "midi.h"
+#include "spi.h"
 #include "delay.h"
 
 #define RANGE 800
 #define ANGLE 90
 
 int main() {
+	uint8_t i = 0;
   system_init();
-  //MIDI_init();
-
-  // Infinite loop
+	SPI_init();
+	
   while (1) {
-    //MIDI_receive();
-    delay_ms(2);
+		SPI_send(i);
+		delay_ms(100);
+		i++;
   }
 
   return 0;
