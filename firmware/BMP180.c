989d0022346a1e08f6579f6286c8d2427b1eb4eb
Jordi Orlando <jordi.orlando@gmail.com>
2016-12-01 15:35:21 -0600

    Make BMP180 variables static/private

--- firmware/BMP180.c
+++ firmware/BMP180.c
@@ -1,9 +1,9 @@
 #include "BMP180.h"
 #include "i2c.h"
 
-int16_t AC1, AC2, AC3, VB1, VB2, MB, MC, MD;
-uint16_t AC4, AC5, AC6;
-double c5, c6, mc, md, x0, x1, x2, y0, y1, y2, p0, p1, p2;
+static int16_t AC1, AC2, AC3, VB1, VB2, MB, MC, MD;
+static uint16_t AC4, AC5, AC6;
+static double c5, c6, mc, md, x0, x1, x2, y0, y1, y2, p0, p1, p2;
 
 // Initialize library for subsequent pressure measurements
 bool BMP180_init(void) {
